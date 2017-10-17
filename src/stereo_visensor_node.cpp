450e17bcfb4fa0e35f568dbdd2308ab029e548c1
lwz <a446516208@163.com>
2017-10-17 15:54:20 +0800

    0.1版本

---
+++ src/stereo_visensor_node.cpp
@@ -0,0 +1,40 @@
+#include "loitor_stereo_visensor_ros/stereo_visensor_cam.h"
+
+
+int main(int argc, char**argv)
+{
+  ros::init(argc, argv, "loitor_stereo_visensor");
+  ros::NodeHandle nh;
+  
+  if(argc != 2)
+	std::cout << "please input the path:Loitor_VISensor_Setups.txt" << std::endl;
+
+    StereoVisensorCam test(nh,argv[1]);
+  
+  /*
+  //ros::NodeHandle n("~/he");
+  message_filters::Subscriber<sensor_msgs::Image> left_sub(nh, "/camera/left/image_raw", 1);
+  message_filters::Subscriber<sensor_msgs::Image> right_sub(nh, "camera/right/image_raw", 1);
+  
+  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> sync_pol;
+  message_filters::Synchronizer<sync_pol> sync(sync_pol(10), left_sub,right_sub);
+  sync.registerCallback(&Stereo);
+  
+  */
+  
+  if(!test.startCam())
+    std::cout << "ERROR: camera open failed" << std::endl;
+  if(!test.startImu())
+    std::cout << "ERROR: imu open failed" << std::endl;
+  
+  test.exectu();
+  
+  //test.img_data_stream();
+  //test.imu_data_stream();
+  /* close cameras */
+  //   visensor_Close_Cameras();
+  /* close IMU */
+  //  visensor_Close_IMU();
+  
+  return 0;
+}
